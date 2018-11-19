dcae2d7ca0bf5422b06f6267aa7cc175ea2a19e0
zigler zhang <zigler@zzqnX1>
2018-11-19 21:13:52 +0800

    add support for multiple sub-domains

--- ddns.py
+++ ddns.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 
 import asyncio
-import os
 import json
 import signal
 import functools
@@ -13,46 +12,58 @@ from urllib import request, error, parse
 from config import read_config, save_config, check_config, cfg
 from get_ip import get_ip
 
+
 def header():
     h = {
         'User-Agent': 'Client/0.0.1 ({})'.format(cfg['email'])
     }
     return h
 
-def get_record_id(domain, sub_domain):
+
+def get_record_ids():
+    sub_domains = cfg['sub_domain'].split(",")
     url = 'https://dnsapi.cn/Record.List'
     params = parse.urlencode({
         'login_token': cfg['login_token'],
         'format': 'json',
-        'domain': domain
+        'domain': cfg['domain']
     })
-    req = request.Request(url=url, data=params.encode('utf-8'), method='POST', headers=header())
+    req = request.Request(
+        url=url, data=params.encode('utf-8'), method='POST', headers=header())
     try:
         resp = request.urlopen(req).read().decode()
     except (error.HTTPError, error.URLError, socket.timeout):
         return None
     records = json.loads(resp).get('records', {})
     for item in records:
-        if item.get('name') == sub_domain:
-            return item.get('id')
+        sd_name = item.get('name')
+        if sd_name in sub_domains:
+            cfg[sd_name] = item.get('id')
+            logging.info(
+                "get record_id: %s for sub_domain %s" % (
+                    item.get('id'), sd_name))
     return None
 
 
-def update_record():
-    url = 'https://dnsapi.cn/Record.Ddns'
-    params = parse.urlencode({
-        'login_token': cfg['login_token'],
-        'format': 'json',
-        'domain': cfg['domain'],
-        'sub_domain': cfg['sub_domain'],
-        'record_id': cfg['record_id'],
-        'record_line': '默认'
-    })
-    req = request.Request(url=url, data=params.encode('utf-8'), method='POST', headers=header())
-    resp = request.urlopen(req).read().decode()
-    records = json.loads(resp)
-    cfg['last_update_time'] = str(time.gmtime())
-    logging.info("record updated: %s" % records)
+def update_records():
+    for sd_name in cfg['sub_domain'].split(","):
+        url = 'https://dnsapi.cn/Record.Ddns'
+        params = parse.urlencode({
+            'login_token': cfg['login_token'],
+            'format': 'json',
+            'domain': cfg['domain'],
+            'sub_domain': sd_name,
+            'record_id': cfg[sd_name],
+            'record_line': '默认'
+        })
+        print(params)
+        req = request.Request(
+            url=url, data=params.encode('utf-8'),
+            method='POST', headers=header())
+        resp = request.urlopen(req).read().decode()
+        records = json.loads(resp)
+        cfg['last_update_time'] = str(time.gmtime())
+        logging.info("record updated: %s" % records)
 
 
 # async def main():
@@ -63,7 +74,6 @@ def main():
             # 对于拥有多个出口 IP 负载均衡的服务器，上面的 get_ip() 函数会在几个 ip 之间不停切换
             # 然后频繁进入这个判断，进行 update_record()，然后很快就会触发 API Limited 了
             # 于是建立一个IP池记载这个服务器的几个出口IP，以免频繁切换
-            
             ip_count = int(cfg['ip_count'])
             ip_pool = cfg['ip_pool'].split(',')[:ip_count]
             cfg['current_ip'] = current_ip
@@ -73,7 +83,7 @@ def main():
                 
                 ip_pool.insert(0, current_ip)
                 cfg['ip_pool'] = ','.join([str(x) for x in ip_pool[:ip_count]])
-                update_record()
+                update_records()
                 save_config()
         else:
             logging.error('get current ip FAILED.')
@@ -94,8 +104,7 @@ if __name__ == '__main__':
     logging.info('start...')
     read_config()
     check_config()
-    cfg['record_id'] = get_record_id(cfg['domain'], cfg['sub_domain'])
-    logging.info("get record_id: %s" % str(cfg['record_id']))
+    cfg['record_id'] = get_record_ids()
     logging.info("watching ip for ddns: %s.%s" % (cfg['sub_domain'], cfg['domain']))
 
     loop = asyncio.get_event_loop()
